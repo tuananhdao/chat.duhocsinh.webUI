9d1d695d0df1a84d39aedcfcc9463716bc0c2eb2
kitsudo1412 <kitsudo1412@gmail.com>
2023-11-05 23:31:31 +0100

    fix grammar

--- prompts/example-prompts.ts
+++ prompts/example-prompts.ts
@@ -2,6 +2,6 @@ export const prompt = ``;
 
 export const samplePhrases = [
   "How much does it cost to study a Master's program in Sweden?",
-  "How income taxes are calculated in Sweden?",
+  "How are income taxes calculated in Sweden?",
   "What are the conditions for permanent residence permit in Sweden?",
 ];
